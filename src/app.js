9633bce9b1092061e9ba27cd2e5530f16faed3df
Wes Forsythe <wes.forsythe@ruralsource.com>
2020-01-07 15:29:19 -0600

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ const forecast = require('./utils/forecast');
 
 const app = express()
 
+//get port from OS
+const port = process.env.PORT || 3000
+
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -98,6 +101,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port + '.')
 })
