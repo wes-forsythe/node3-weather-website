340933629b05b2da08b7ddb1bc1ed2b508fa6090
Wes Forsythe <wes.forsythe@ruralsource.com>
2020-01-07 16:17:30 -0600

    Add high and low temps to the forcast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,9 +9,10 @@ const forecast = (latitude, longitude, callback) => {
             callback(body.error, undefined)
         } else {
             const temp = " It is currently " + body.currently.temperature + " degrees out. "
+            const minMax = "The high today is " + body.daily.data[0].temperatureHigh + " degrees with a low of " + body.daily.data[0].temperatureLow + " degrees."
             const rain = "There is a " + (body.currently.precipProbability * 100) + "% chance of rain."
             const summary = body.daily.data[0].summary
-            const forecastSummary = summary + temp + rain;
+            const forecastSummary = summary + temp + minMax + rain;
             callback(undefined, forecastSummary)
         }
     });
